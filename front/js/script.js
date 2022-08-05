7c7f6db6fb6f242b1df6834817d43d3008ab79ce
Hadrien Borie <borie.hadrien@gmail.com>
2022-08-05 15:31:43 +0200

    add note / correction

--- front/js/script.js
+++ front/js/script.js
@@ -1,21 +1,22 @@
+/*Récupération des données via l'API*/
 fetch("http://localhost:3000/api/products")
-    .then(function(response){
+    .then(function (response) {
         return response.json();
     })
-        .then(function(listSofa){
-            const articles = document.getElementById("items")
-        
+    .then(function (listSofa) {
+        const ARTICLES = document.getElementById("items")/*recherche de l'élément par son id*/
 
+        for (const element of listSofa) {/*On récupère tous les élements de l'API*/
+            const SOFA = element;
 
-    for (const element of listSofa) {
-        const sofa = element;
+            let text = `<a href="./product.html?id=${SOFA._id}"> 
+                        <article> <img src="${SOFA.imageUrl}" alt="${SOFA.altTxt}"> 
+                        <h3 class="productName"> ${SOFA.name} </h3> 
+                        <p class="productDescription"> ${SOFA.description}</p></article>`;
+            ARTICLES.innerHTML += text;/*on implémente tous les éléments de chaque article*/
+        }
+    })
 
-        let text = `<a href="./prodct01.html?id=${sofa._id}"> <article> <img src="${sofa.imageUrl}" alt="${sofa.altTxt}"> <h3 class="productName"> ${sofa.name} </h3> <p class="productDescription"> ${sofa.description}</p></article>`;
-        articles.innerHTML += text;
-    }
-
-
-})
-.catch(function(error){
-    console.error(error)
-});
+    .catch(function (error) {
+        console.error(error)
+    });
