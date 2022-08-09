7f554c2c78f057377f23617853a75f4e10978fa8
Hadrien Borie <borie.hadrien@gmail.com>
2022-08-09 15:13:12 +0200

    details

--- front/js/script.js
+++ front/js/script.js
@@ -4,16 +4,16 @@ fetch("http://localhost:3000/api/products")
         return response.json();
     })
     .then(function (listSofa) {
-        const ARTICLES = document.getElementById("items")/*recherche de l'élément par son id*/
+        const articles = document.getElementById("items")/*recherche de l'élément par son id*/
 
         for (const element of listSofa) {/*On récupère tous les élements de l'API*/
-            const SOFA = element;
+            const sofa = element;
 
-            let text = `<a href="./product.html?id=${SOFA._id}"> 
-                        <article> <img src="${SOFA.imageUrl}" alt="${SOFA.altTxt}"> 
-                        <h3 class="productName"> ${SOFA.name} </h3> 
-                        <p class="productDescription"> ${SOFA.description}</p></article>`;
-            ARTICLES.innerHTML += text;/*on implémente tous les éléments de chaque article*/
+            let text = `<a href="./product.html?id=${sofa._id}"> 
+                        <article> <img src="${sofa.imageUrl}" alt="${sofa.altTxt}"> 
+                        <h3 class="productName"> ${sofa.name} </h3> 
+                        <p class="productDescription"> ${sofa.description}</p></article>`;
+            articles.innerHTML += text;/*on implémente tous les éléments de chaque article*/
         }
     })
 
